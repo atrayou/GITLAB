0dc7ad27fba661578159742227f332743ba15c84
atranouz <eric.tran96@laposte.net>
2016-04-27 01:59:36 +0700

    Modified the display of square.c

--- square.c
+++ square.c
@@ -9,10 +9,10 @@ int main(int argc, char *argv[])
   }
   int nb = 0;
   sscanf(argv[1], "%d", &nb);
-  if(nb == 0) {
+  if(!nb) {
     fprintf(stderr, "Enter a positive number\n");
     exit(EXIT_FAILURE);
   }
-  printf("SQUARE: %d\n", nb * nb);
+  printf("%s * %s = %d\n", argv[1], argv[1], nb * nb);
   return EXIT_SUCCESS;
 }
